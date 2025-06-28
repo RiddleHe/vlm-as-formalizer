842dd745528dd3546d29b9c379152fa69590de4a
AsukaRedpanda <odysseusailoon@gmail.com>
2025-06-28 13:31:23 -0700

    test out plain vlm

--- test_vlm.py
+++ test_vlm.py
@@ -5,7 +5,7 @@ import torchvision.transforms as T
 from decord import VideoReader, cpu
 from PIL import Image
 from torchvision.transforms.functional import InterpolationMode
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer, AutoConfig
 
 IMAGENET_MEAN = (0.485, 0.456, 0.406)
 IMAGENET_STD = (0.229, 0.224, 0.225)
